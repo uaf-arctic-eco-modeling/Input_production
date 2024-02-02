3d2036f3728b679e836b03d5d3cbbbd72efe1718
Helene Genet <hgenet@alaska.edu>
2024-02-02 08:36:36 -0900

    Formating variables to match dvmdostem input requirement

--- site_ancillary_input_formating.py
+++ site_ancillary_input_formating.py
@@ -77,6 +77,13 @@ topo_nc['y']=(['Y'],  np.array([float(y)]))
 topo_nc['x']=(['X'],  np.array([float(x)]))
 topo_nc['lat'] = (['Y','X'],  np.array([[site_lat]]))
 topo_nc['lon'] = (['Y','X'],  np.array([[site_lon]]))
+topo_nc['y'] = topo_nc['y'].astype(np.intc)
+topo_nc['x'] = topo_nc['x'].astype(np.intc)
+topo_nc['lat'] = topo_nc['lat'].astype(np.single)
+topo_nc['lon'] = topo_nc['lon'].astype(np.single)
+topo_nc['slope'] = topo_nc['slope'].astype(np.double)
+topo_nc['aspect'] = topo_nc['aspect'].astype(np.double)
+topo_nc['elevation'] = topo_nc['elevation'].astype(np.double)
 
 topo_nc['lat'].attrs={'standard_name':'latitude','units':'degree_north'}
 topo_nc['lon'].attrs={'standard_name':'longitude','units':'degree_east'}
@@ -86,7 +93,7 @@ topo_nc['aspect'].attrs={'standard_name':'aspect','units':'degree','grid_mapping
 topo_nc['elevation'].attrs={'standard_name':'elevation','units':'m','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
 topo_nc['slope'].attrs={'standard_name':'slope','units':'degree','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
 
-topo_nc.to_netcdf(os.path.join(indir,site,'topo.nc'),unlimited_dims='time')
+topo_nc.to_netcdf(os.path.join(indir,site,'topo.nc'))
 
 
 
@@ -103,6 +110,13 @@ text_nc['y']=(['Y'],  np.array([float(y)]))
 text_nc['x']=(['X'],  np.array([float(x)]))
 text_nc['lat'] = (['Y','X'],  np.array([[site_lat]]))
 text_nc['lon'] = (['Y','X'],  np.array([[site_lon]]))
+text_nc['y'] = text_nc['y'].astype(np.intc)
+text_nc['x'] = text_nc['x'].astype(np.intc)
+text_nc['lat'] = text_nc['lat'].astype(np.single)
+text_nc['lon'] = text_nc['lon'].astype(np.single)
+text_nc['pct_sand'] = text_nc['pct_sand'].astype(np.single)
+text_nc['pct_silt'] = text_nc['pct_silt'].astype(np.single)
+text_nc['pct_clay'] = text_nc['pct_clay'].astype(np.single)
 
 text_nc['lat'].attrs={'standard_name':'latitude','units':'degree_north'}
 text_nc['lon'].attrs={'standard_name':'longitude','units':'degree_east'}
@@ -112,7 +126,7 @@ text_nc['pct_sand'].attrs={'standard_name':'sand','units':'pct','grid_mapping':'
 text_nc['pct_silt'].attrs={'standard_name':'silt','units':'pct','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
 text_nc['pct_clay'].attrs={'standard_name':'clay','units':'pct','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
 
-text_nc.to_netcdf(os.path.join(indir,site,'soil-texture.nc'),unlimited_dims='time')
+text_nc.to_netcdf(os.path.join(indir,site,'soil-texture.nc'))
 
 
 
@@ -132,6 +146,14 @@ fri_nc['y']=(['Y'],  np.array([float(y)]))
 fri_nc['x']=(['X'],  np.array([float(x)]))
 fri_nc['lat'] = (['Y','X'],  np.array([[site_lat]]))
 fri_nc['lon'] = (['Y','X'],  np.array([[site_lon]]))
+fri_nc['y'] = fri_nc['y'].astype(np.intc)
+fri_nc['x'] = fri_nc['x'].astype(np.intc)
+fri_nc['lat'] = fri_nc['lat'].astype(np.single)
+fri_nc['lon'] = fri_nc['lon'].astype(np.single)
+fri_nc['fri'] = fri_nc['fri'].astype(np.intc)
+fri_nc['fri_severity'] = fri_nc['fri_severity'].astype(np.intc)
+fri_nc['fri_jday_of_burn'] = fri_nc['fri_jday_of_burn'].astype(np.intc)
+fri_nc['fri_area_of_burn'] = fri_nc['fri_area_of_burn'].astype(np.intc)
 
 fri_nc['lat'].attrs={'standard_name':'latitude','units':'degree_north'}
 fri_nc['lon'].attrs={'standard_name':'longitude','units':'degree_east'}
@@ -141,7 +163,7 @@ fri_nc['fri'].attrs={'standard_name':'fire_return_interval','units':'yrs','grid_
 fri_nc['fri_jday_of_burn'].attrs={'standard_name':'fri_jday_of_burn','units':'doy','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
 fri_nc['fri_area_of_burn'].attrs={'standard_name':'fri_area_of_burn','units':'','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
 
-fri_nc.to_netcdf(os.path.join(indir,site,'fri-fire.nc'),unlimited_dims='time')
+fri_nc.to_netcdf(os.path.join(indir,site,'fri-fire.nc'))
 
 
 
@@ -158,6 +180,11 @@ veg_nc['y']=(['Y'],  np.array([float(y)]))
 veg_nc['x']=(['X'],  np.array([float(x)]))
 veg_nc['lat'] = (['Y','X'],  np.array([[site_lat]]))
 veg_nc['lon'] = (['Y','X'],  np.array([[site_lon]]))
+veg_nc['y'] = veg_nc['y'].astype(np.intc)
+veg_nc['x'] = veg_nc['x'].astype(np.intc)
+veg_nc['lat'] = veg_nc['lat'].astype(np.single)
+veg_nc['lon'] = veg_nc['lon'].astype(np.single)
+veg_nc['veg_class'] = veg_nc['veg_class'].astype(np.intc)
 
 veg_nc['lat'].attrs={'standard_name':'latitude','units':'degree_north'}
 veg_nc['lon'].attrs={'standard_name':'longitude','units':'degree_east'}
@@ -165,7 +192,7 @@ veg_nc['y'].attrs={'standard_name':'projection_y_coordinate','long_name':'y coor
 veg_nc['x'].attrs={'standard_name':'projection_x_coordinate','long_name':'x coordinate of projection','units':'m'}
 veg_nc['veg_class'].attrs={'standard_name':'veg_class','units':'yrs','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
 
-veg_nc.to_netcdf(os.path.join(indir,site,'vegetation.nc'),unlimited_dims='time')
+veg_nc.to_netcdf(os.path.join(indir,site,'vegetation.nc'))
 
 
 
@@ -182,6 +209,11 @@ drain_nc['y']=(['Y'],  np.array([float(y)]))
 drain_nc['x']=(['X'],  np.array([float(x)]))
 drain_nc['lat'] = (['Y','X'],  np.array([[site_lat]]))
 drain_nc['lon'] = (['Y','X'],  np.array([[site_lon]]))
+drain_nc['y'] = drain_nc['y'].astype(np.intc)
+drain_nc['x'] = drain_nc['x'].astype(np.intc)
+drain_nc['lat'] = drain_nc['lat'].astype(np.single)
+drain_nc['lon'] = drain_nc['lon'].astype(np.single)
+drain_nc['drainage_class'] = drain_nc['drainage_class'].astype(np.int_)
 
 drain_nc['lat'].attrs={'standard_name':'latitude','units':'degree_north'}
 drain_nc['lon'].attrs={'standard_name':'longitude','units':'degree_east'}
@@ -189,8 +221,35 @@ drain_nc['y'].attrs={'standard_name':'projection_y_coordinate','long_name':'y co
 drain_nc['x'].attrs={'standard_name':'projection_x_coordinate','long_name':'x coordinate of projection','units':'m'}
 drain_nc['drainage_class'].attrs={'standard_name':'drainage_class','units':'yrs','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
 
-drain_nc.to_netcdf(os.path.join(indir,site,'drainage.nc'),unlimited_dims='time')
+drain_nc.to_netcdf(os.path.join(indir,site,'drainage.nc'))
 
+
+
+
+### runmask data
+
+mask = pd.DataFrame()
+mask['Y'] = [int(Y)]
+mask['X'] = [int(X)]
+mask['run'] = 1
+mask_nc = mask.set_index(['Y', 'X']).to_xarray()
+mask_nc['y']=(['Y'],  np.array([float(y)]))
+mask_nc['x']=(['X'],  np.array([float(x)]))
+mask_nc['lat'] = (['Y','X'],  np.array([[site_lat]]))
+mask_nc['lon'] = (['Y','X'],  np.array([[site_lon]]))
+mask_nc['y'] = mask_nc['y'].astype(np.intc)
+mask_nc['x'] = mask_nc['x'].astype(np.intc)
+mask_nc['lat'] = mask_nc['lat'].astype(np.single)
+mask_nc['lon'] = mask_nc['lon'].astype(np.single)
+mask_nc['run'] = mask_nc['lon'].astype(np.int_)
+
+mask_nc['lat'].attrs={'standard_name':'latitude','units':'degree_north'}
+mask_nc['lon'].attrs={'standard_name':'longitude','units':'degree_east'}
+mask_nc['y'].attrs={'standard_name':'projection_y_coordinate','long_name':'y coordinate of projection','units':'m'}
+mask_nc['x'].attrs={'standard_name':'projection_x_coordinate','long_name':'x coordinate of projection','units':'m'}
+mask_nc['run'].attrs={'standard_name':'mask','units':'','grid_mapping':'albers_conical_equal_area','_FillValue': -999.0}
+
+mask_nc.to_netcdf(os.path.join(indir,site,'run-mask.nc'))
 
 
 
