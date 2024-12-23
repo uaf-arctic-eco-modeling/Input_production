201b7852e3b3fad59a8e640d4d120d4f93e3e41b
Tobey Carman <tobeycarman@gmail.com>
2024-12-23 11:11:34 -0900

    Move function for actually generating the mask from shapefiles to AOIMask module.
    Left this outside the class because with a little adjustment to the
    parameters (pass in the directories/files to write) the function can be
    used without an class instance.

--- source/AOIMask.py
+++ source/AOIMask.py
@@ -12,7 +12,64 @@ from osgeo import gdal
 import util
 
 
+def merge_and_buffer_shapefiles(global_political_map, eco_region_map):
+  '''
+  Creates two shape files in different projections that cover the whole
+  area of interest. Each file is a single feature (not sure if this is the
+  right term?) with a bunch of polygons defining the outline of the AOI.
+
+  Writes various files to disk. 
+
+  Parameters
+  ==========
+  global_political_map : str
+      path to the global political shapefile
+  eco_region_map: str
+      path to the eco regeion shapefile
+
+  Returns
+  ========
+  None
+  '''
+  # Read the eco region shape file, extract the shapes of interest, and then
+  # merge (dissolve) them into one single shape (polygon?)
+  print(f"Opening {eco_region_map=}...")
+  erm = gpd.read_file(eco_region_map)
+
+  eco_tundra = erm[(erm['BIOME_NAME'] == 'Tundra') | (erm['BIOME_NAME'] == 'Boreal Forests/Taiga')]
+  eco_north = eco_tundra[(eco_tundra['REALM'] != 'Antarctica') & (eco_tundra['REALM'] != 'Australasia')]
+
+  # Dissolve geometries within `groupby` into single observation
+  eco_north = eco_north.dissolve() 
+
+  # Read the global map, 
+  print(f"Opening {global_political_map=}...")
+  gpm = gpd.read_file(global_political_map)
+  ak_greenland = gpm[(gpm['shapeName']=='Alaska') | (gpm['shapeGroup']=='GRL')]
+  ak_greenland = ak_greenland.dissolve()
+  ak_greenland.to_crs(eco_north.crs)
+
+  AOI = eco_north.union(ak_greenland, align=True)
+
+  util.mkdir_p('working/aoi_4326/')
+  util.mkdir_p('working/aoi_6931/')
+  util.mkdir_p('working/aoi_5km_buffer_6931/')
+
+  print("Writing AOI files...")
+  AOI.to_crs(4326).to_file('working/aoi_4326/aoi_4326.shp')
+  AOI.to_crs(6931).to_file('working/aoi_6931/aoi_6931.shp')
+
+  AOI_5km_buffer = AOI.to_crs(6931).buffer(5000) # 5km
+  AOI_5km_buffer.tmp = 1 # ?? what is this for?
+  print("Writing buffered AOI file...")
+  AOI_5km_buffer.to_file('working/aoi_5km_buffer_6931/aoi_5km_buffer_6931.shp')
+
+
+
 class AOIMask(object):
+  '''
+  Object that encapsulates an Area of Interest Mask.
+  '''
   def __init__(self):
     self.politic_map_fname = "geoBoundariesCGAZ_ADM1.zip"
     self.politic_map_url = f"https://github.com/wmgeolab/geoBoundaries/raw/main/releaseData/CGAZ/{self.politic_map_fname}"
@@ -54,16 +111,14 @@ class AOIMask(object):
       print(f"Extracting {x=}")
       zip_ref.extractall(x)
 
+  # def create_from_scratch(self):
+  #   self._download()
+  #   self._unzip()
+  #   self.create_from_shapefiles():
 
-
-
-
-
-
-  def create_from_scratch(self):
-    self._download()
-    self._unzip()
-    pass
+  def create_from_shapefiles(self):
+    merge_and_buffer_shapefiles(pathlib.Path(self.root, self.politic_map_fname),
+                                pathlib.Path(self.root, self.eco_map_fname))
 
   def load_from_raster(self, raster_file):
     self.aoi_raster = gdal.Open(raster_file,  gdal.gdalconst.GA_ReadOnly)
