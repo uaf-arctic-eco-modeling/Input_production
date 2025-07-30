3a2f53e3017050858570ed92a138e4766a65faeb
Rawser Spicer <rwspicer@alaska.edu>
2025-07-30 11:14:13 -0800

    fixes error names; comments out potentally unused errors

--- src/temds/datasources/errors.py
+++ src/temds/datasources/errors.py
@@ -5,32 +5,32 @@ Errors
 custom exceptions for temds.datasources
 """
 
-class TEMDatasetUninitializeError(Exception):
+class TEMDatasetUninitializedError(Exception):
     """Raised if dataset is None and a base function is called"""
     pass
 
-class TEMDatasetMissingResolutionError(Exception):
-    """Raised if dataset has unset resolution and the resolution is required to
-    proceed.
-    """
-    pass
+# class TEMDatasetMissingResolutionError(Exception):
+#     """Raised if dataset has unset resolution and the resolution is required to
+#     proceed.
+#     """
+#     pass
 
-class AnnualDailyContinuityError(Exception):
-    """Raise when if the there is a missing year in..."""
-    pass
+# class AnnualDailyContinuityError(Exception):
+#     """Raise when if the there is a missing year in..."""
+#     pass
 
-class InvalidCalendarError(Exception):
-    """Raise when the calendar attribute of the time dimension of the dataset is not 365_day or noleap"""
-    pass
+# class InvalidCalendarError(Exception):
+#     """Raise when the calendar attribute of the time dimension of the dataset is not 365_day or noleap"""
+#     pass
 
-class AnnualDailyYearMismatchError(Exception):
-    """Raise when the year of the file does not match the year of the object"""
-    pass
+# class AnnualDailyYearMismatchError(Exception):
+#     """Raise when the year of the file does not match the year of the object"""
+#     pass
 
-class AnnualDailyYearUnknownError(Exception):
-    """Raise when self.year is unkonwn and cannot be loaded."""
-    pass
+# class AnnualDailyYearUnknownError(Exception):
+#     """Raise when self.year is unkonwn and cannot be loaded."""
+#     pass
 
-class AnnualTimeSeriesError(Exception):
-    """Raise when for errors related to annual time series data."""
-    pass
+# class AnnualTimeSeriesError(Exception):
+#     """Raise when for errors related to annual time series data."""
+#     pass
