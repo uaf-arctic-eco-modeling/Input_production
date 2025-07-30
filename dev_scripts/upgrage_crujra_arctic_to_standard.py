bbd7dbb20bf3f2bf5607661765eeac316fe6464a
Rawser Spicer <rwspicer@alaska.edu>
2025-07-30 14:14:44 -0800

    added script to update cru data

---
+++ dev_scripts/upgrage_crujra_arctic_to_standard.py
@@ -0,0 +1,21 @@
+"""Script to convet crujra preprocessed arctic data to
+new standard units 
+"""
+
+from temds.datasources import dataset
+from temds.logger import Logger, INFO
+from pathlib import Path
+import sys
+my_logger = Logger(verbose_levels = INFO)
+
+try: 
+    in_path = sys.argv[1]
+except IndexError:
+    print("call script like: python upgrade_crujra_arctic_to_standard.py path/to/crujra-arctic")
+
+for file in sorted(Path(in_path ).glob('*.nc')):
+    my_logger.info(f'Processing File: {file}')
+    corrected = dataset.YearlyDataset.from_preprocessed_crujra(file, logger=my_logger)
+    out_path = Path(file).parent.parent.joinpath('cru-jra-standard', file.name)
+    my_logger.info(f'Saving File: {out_path}')
+    corrected.save(out_path)
