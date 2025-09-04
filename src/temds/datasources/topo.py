676b8a6b78227d6ca527392bd4b31d1a99b8207b
Tobey Carman <tcarman2@alaska.edu>
2025-09-04 12:23:48 -0800

    Notes.

--- src/temds/datasources/topo.py
+++ src/temds/datasources/topo.py
@@ -9,23 +9,14 @@ See: for dataset details
 
 NAME = 'topo'
 
-
 # citation for topo  dataset
 CITATION = ()
 
 url = 'https://edcintl.cr.usgs.gov/downloads/sciweb1/shared/topo/downloads/GMTED/Grid_ZipFiles/mn75_grd.zip'
 
+# This is an ArcInfo Binary Grid format. Lots of files and folders inside...
 zipped_raw = 'mn75_grd.zip'
+
+
 unzipped_raw = 'mn75_grd/'
-
-
-#raw_file = 'mn75_grd'
-
-#unzipped_raw = 'mn75_grd'
-
-#processed = 'working/06-ancillary/elevation_250m_4326.tif'
-
-class TopoURLError(Exception):
-    """Raised if the url cannot be formatted"""
-    pass
 
