fb00879ed8d85ae14f8bc0d6a46163063903f8c5
Rawser Spicer <rwspicer@alaska.edu>
2025-09-29 13:00:35 -0800

    added psl_to_vapo callback

--- src/temds/datasources/cmip6.py
+++ src/temds/datasources/cmip6.py
@@ -24,7 +24,7 @@ climate_variables.register('tair_avg', NAME, 'tas')
 climate_variables.register('prec', NAME, 'pr')
 climate_variables.register('nirr', NAME, 'rsds')
 climate_variables.register('spfh', NAME, 'huss')
-climate_variables.register('pres', NAME, 'psl')
+climate_variables.register('psl', NAME, 'psl')
 
 climate_variables.register_source_unit('tair_avg', NAME, Unit('kelvin'))
 
@@ -40,3 +40,26 @@ DEFAULT_EXPERIMENTS = ['historical', 'ssp126', 'ssp245', 'ssp370', 'ssp585']
 DEFAULT_ENSEMBLES = ['r1i1p1f1']  # was 'r1i1p1f1_gn' what is gn?
 DEFAULT_MODELS=['ACCESS-CM2','MRI-ESM2-0']
 
+def callback_psl_to_vapo(dataset, logger, **kwargs):
+    func_name = 'cmip6.callback_psl_to_vapo'
+    elevation = kwargs['elevation']
+ 
+    logger.info(f'{func_name}: Calculating pres from psl, elevation and, air temp')
+    psl = dataset['psl']
+    try:
+        tas = dataset['tas'] 
+    except KeyError:
+        tas = dataset['tair_avg']
+    pres = climate_variables.calculate_pres_from_psl(psl,tas, elevation)
+    try:
+        spfh = dataset['huss']
+    except KeyError:
+        spfh = dataset['spfh']
+
+    logger.info(f'{func_name}: Calculating vapo kPa')
+    dataset['vapo'] = climate_variables.calculate_vapo(pres, spfh)
+    unit = climate_variables.CLIMATE_VARIABLES['vapo'].std_unit.name
+    v_name = climate_variables.CLIMATE_VARIABLES['vapo'].name
+    dataset['vapo'].attrs.update(units=unit, name=v_name)
+    return dataset
+    
