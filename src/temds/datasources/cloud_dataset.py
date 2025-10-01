e7e75cf9692a3336bd9e0688a4e70128b9adadde
Rawser Spicer <rwspicer@alaska.edu>
2025-10-01 15:22:51 -0800

    added download to cloud dataset

--- src/temds/datasources/cloud_dataset.py
+++ src/temds/datasources/cloud_dataset.py
@@ -1,7 +1,9 @@
 from .dataset import TEMDataset
 from temds.logger import Logger
+from temds import gcloud_tools
 import numpy as np
 import time
+from pathlib import Path
 
 import ee
 
@@ -9,7 +11,7 @@ sample_dataset = "ECMWF/ERA5_LAND/DAILY_AGGR"
 sample_bands=bands = ['temperature_2m', 'dewpoint_temperature_2m', 'total_precipitation_sum',  "surface_solar_radiation_downwards_sum", ]
 
 
-class CloudDataset(TEMDataset):
+class CloudDataset(object):
 
     def __init__(self, dataset, bands, logger=Logger(), **kwargs):
         """
@@ -25,15 +27,15 @@ class CloudDataset(TEMDataset):
         **kwargs:
             Key word arguments passed to `load` 
         """
-        self.dataset = dataset # GEE image_collection i.e "ECMWF/ERA5_LAND/DAILY_AGGR"
+        self.dataset = ee.ImageCollection(dataset) # GEE image_collection i.e "ECMWF/ERA5_LAND/DAILY_AGGR"
         self.bands = bands # list or str, of bands in dataset
         self.logger = logger
         self._cached_load_kwargs={}
 
 
-    def download(self, where, bounds, name='temp-data', grive_location='colud_dataset_temp'):
+    def download(self, where, bounds, credentials, filter_func=lambda x: x, name='temp-data', gdrive_location='colud_dataset_temp'):
         
-        ic = ee.ImageCollection(self.dataset)
+        # ic = self.dataset
         # gee_aoi=ee.Geometry.Rectangle([[a.x,a.y],[b.x,b.y]])
         
        
@@ -43,14 +45,14 @@ class CloudDataset(TEMDataset):
         tasks = []
         files = []
         for band in self.bands:
-            daily = ic.filterDate("2020-01-01T00:00", "2021-01-01T00:00").select(band)
+            daily = filter_func(self.dataset).select(band)
             merged = daily.toBands()
             merged.bandNames().size().getInfo()
             file_name = f'{name}-{band}'
             task = ee.batch.Export.image.toDrive(
                 image=merged,
                 description=file_name,
-                folder= grive_location,
+                folder= gdrive_location,
                 region=gee_aoi,
                 scale=4000,
                 # crsTransform=[30, 0, -2493045, 0, -30, 3310005],
@@ -65,3 +67,13 @@ class CloudDataset(TEMDataset):
                 raise ValueError('Check the cloud console for errors')
             time.sleep(10)
 
+        parent_id = Path(tasks[0].status()['destination_uris'][0]).name
+        files = gcloud_tools.list_files(credentials, parent_id)
+        for file in files:
+            if not name in file['name']:
+                continue
+            gcloud_tools.download_file(
+                credentials, file['id'], Path(where).joinpath(file['name'])
+            )
+
+        
