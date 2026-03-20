f9165a84033c90dbe1f40033f53bc29c864580a8
Rawser Spicer <rwspicer@alaska.edu>
2026-03-20 13:46:36 -0800

    added a bunch of features to create, save, import data, and load Regions, Masks, and Mainfests

--- src/temds/region/region.py
+++ src/temds/region/region.py
@@ -1,65 +1,219 @@
 """
+Region
+------
+
+Objects for representing regions. The Region and SubregionGenerator classes
+are designed as a replacement for earlier tile, and aoi related classes and
+concepts.
+
+TODO:
+- implement apply_mask
+- logger integration
+- document import_datasource kwargs
+- port other tile features over
 
 """
-# https://gcmeval.met.no/
 from pathlib import Path
 
 import shapely
 import geopandas as gpd
 from osgeo import gdal
+import pyproj
 import yaml
-from collections import UserDict
+
 
 
 from ..gdal_tools import empty_dataset
 
-from .logger import Logger
-from .datasources import dataset, timeseries
+from ..logger import Logger
+from ..datasources import dataset, timeseries
 
+from .mask import Mask
+from .manifest import Manifest
+from .tools import mask_boundary_compatibility_report
 
-class Manifest(UserDict):
-    def __init__(self):
-        self.data = {
-            'data': {}
-        }
+class MaskBoundaryCompatibilityError(Exception):
+    """Exception for region mask and  boundary incompatibility errors
+    """
+    pass
 
-    @classmethod
-    def from_file(cls, where):
-        new = cls()
-        with Path(where).open('r') as fd:
-            new.data = yaml.load(fd, yaml.Loader)
-        return new
+class Region(object):
+    """
+        This class represents a region to process data over. A Region has 
+    a `boundary` (geopandas GeoSeries, or GeoDataFrame) and a `mask` (mask,Mask) 
+    which define the spatial extent and properties of the region. A region also 
+    has `data`, data is imported to the region to match the spatial properties 
+    described by the `boundary` and the `mask`. RS_Logger style logging is 
+    supported by this object. See `__init__` for documentation on how to set up 
+    a Region object
+
+        The only required item to create a Region is the boundary, but a mask 
+    may also be provided. When a mask is not provided it is calculated from the 
+    boundary, with all values set to 1, a resolution must be provided in this 
+    case. When a boundary and a mask are provided they are evaluated based on 
+    their CRS, transform and shape, and an Exception is raised if they are not.
+
+    Attributes
+    ----------
+    boundary: geopandas.GeoDataFrame or geopandas.GeoSeries
+        Single row GeoDataFrame, or GeoSeries used as region boundary. 
+        defines crs of data
+    mask: mask.Mask
+        Mask object which defines resolution, transform of data
+    logger: rs_logger.Logger
+        Internal logging object
+    """
+    def __init__(
+            self, boundary: gpd.GeoDataFrame | gpd.GeoSeries , 
+            mask: Mask = None, logger: Logger =Logger(), **kwargs
+        ): 
+        """Create a region
+
+        Parameters
+        ----------
+        boundary: geopandas.GeoDataFrame or geopandas.GeoSeries
+            Single row GeoDataFrame, or GeoSeries used as region boundary. 
+        mask: mask.Mask, Optional
+            Mask object which defines resolution, transform of data.
+            Will be created from boundary in not provided. When not 
+            provided 'resolution` key word argument must be provided.
+            Created mask will assume uniform pixel size, and north
+            up format for raster
+        logger: rs_logger.Logger
+        kwargs:
+            'resolution': number
+                Resolution in boundary.CRS units
         
+        Raises
+        ------
+        MaskBoundaryCompatibilityError
+            Will be raised if the CRS, transform or shapes, of the 
+            boundary or mask are incompatible
+        """
+        self.boundary = boundary.reset_index()
+        if mask:
+            self.mask = mask
+        else:
+            resolution = kwargs['resolution']
+            self.mask = Mask.from_extent(boundary, resolution) 
 
-    def to_file(self, where):
-        with where.open('w') as fd:
-            yaml.safe_dump(self.items, fd, sort_keys=False)
+        self.data = {} 
+        self.logger = logger
+        self.check_mask_compatibility(self.mask)
 
-    # def update(self, new):
-      
 
-class RegionOfInterest(object):
-    def __init__(self, vector, mask, logger=Logger()):
-        self.boundary = vector #GeoDataFrame or GeoSeries with single row limit to single polygon?
-        self.data = {}
-        self.mask = mask
+    def check_mask_compatibility(self, mask):
+        """Checks that a mask is compatible with `boundary`
+        Parameters
+        ----------
+        mask: mask.Mask
+            Mask object
+
+        Raises
+        ------
+        MaskBoundaryCompatibilityError
+            Will be raised if the CRS, transform or shapes, of the 
+            boundary or mask are incompatible
+        """
+        report = mask_boundary_compatibility_report(mask, self.boundary)
+        if not report[0]:
+            raise MaskBoundaryCompatibilityError(
+                '`mask` CRS and `boundary` crs do not match'
+            )
+        if not report[1]:
+            raise MaskBoundaryCompatibilityError(
+                '`mask` shape, and calculated `boundary` shape do not match'
+            )
+        if not report[2]:
+            raise MaskBoundaryCompatibilityError(
+                '`mask` transform , and calculated `boundary` transform do not match'
+            )
 
     @property
-    def resolution(self):
-        """Resolution is defined by the mask dataset
-        """
+    def resolution(self) -> tuple:
+        """Resolution is defined by the mask dataset"""
         return self.mask.resolution
+    
+    @property
+    def crs(self) -> pyproj.CRS:
+        """CRS of region"""
+        return self.boundary.crs
+    
+    @property
+    def transform(self) -> tuple:
+        """Gdal formatted crs"""
+        return self.mask.transform
+    
+    @property
+    def shape(self) -> tuple:
+        """2d shape of region as x,y """
+        return self.mask.shape
 
-    def import_from_directory(self, directory: Path):
+    def apply_mask(self, keys: list = None, no_data_val: int| float = None, mask: Mask=None):
+        """set values to no data where mask (internal, or provided) is == 0
+
+        Parameters
+        ----------
+        keys: list, optional
+            Optional list of items in `data` to apply mask to. When not 
+        provided all items will have mask applied.
+        no_data_val: number, optional
+            No data val to use, When not provided will pull no data from
+            each dataset
+        mask: Mask, Optional
+            A mask to apply, when not provided will use internal `mask` attr.
+            Must be compatible with Boundary
+        
+        Raises
+        ------
+        MaskBoundaryCompatibilityError
+            Will be raised if the CRS, transform or shapes, of the 
+            boundary or mask are incompatible
+
+        """
+        # if keys is None:
+        #     keys = self.data.keys()
+        # if mask is None:
+        #     mask = self.mask
+        # self.check_mask_compatibility(mask)
         pass
 
-    ## rename this
-    def import_from_datasource(self, name, datasource, buffered=True, callback = None, **kwargs):
-        pass
-        """Loads an item to `data` as name from datasource. Each datasource 
-        (e.g. AnnualDaily, WorldClim) needs to implement a get_by_extent() 
-        method that can return an xarray dataset or AnnualTimeseries to a 
-        specified spatial temporal resolution and extent
+    @classmethod
+    def from_directory(cls, directory: Path, logger: Logger = Logger()):
+        """Create a Region from a directory containing a manifest file
+
+        Parameters
+        ----------
+        directory: Path
+            a directory containing a manifest file, and data which the manifest
+            describes
+        logger: rs_logger.Logger
+
+        Returns
+        -------
+        Region
+        """
+        manifest = Manifest.from_file( directory/ 'manifest.yml' )
+        boundary = gpd.read_file(directory / manifest['boundary'] )
+        mask = Mask.from_file(directory / manifest['mask'])
+        new = cls(boundary, mask, logger)
+
+        for item, _file in manifest['data'].items():
+            in_path = Path(directory).joinpath(_file)
+            if in_path.is_dir():
+                new.data[item] = timeseries.YearlyTimeSeries(
+                    in_path, 
+                    logger = new.logger
+                )
+            else:
+                new.data[item] = dataset.TEMDataset(
+                    in_path, logger = new.logger
+                )
+        return new
+
+    def import_datasource(self, name, datasource, callback = None, **kwargs):
+        """Loads an item to `data` as name from a datasource. Each datasource 
+        may be a ...
 
         Parameters
         ----------
@@ -69,25 +223,23 @@ class RegionOfInterest(object):
             Object must implement `get_by_extent` with 6 arguments
             (minx: float, maxx: float, miny: float, maxy: float, 
             extent_crs: pyproj.crs.crs.CRS, resolution: float)
-
-        buffered: bool, Defaults True
-            When true add buffer to tile data being clipped
+        callback: function, Optional
+            Call back to apply to data after loading is performed
+        **kwargs:
+            
         """
-        minx, miny, maxx, maxy = self.extent[
+        minx, miny, maxx, maxy = self.boundary.bounds[
             ['minx', 'miny', 'maxx', 'maxy']
         ].iloc[0]
-        extent = minx, miny, maxx, maxy 
-        if buffered:
-            minx,maxx = minx-self.buffer_area,maxx+self.buffer_area
-            miny,maxy = miny-self.buffer_area,maxy+self.buffer_area
         
 
         kwargs['resolution'] = self.resolution
 
 
         self.logger.info(
-            f'importing {name} from {datasource} for the extent: {extent}'
+            f'importing {name} from {datasource} for the extent: {minx}, {miny}, {maxx}, {maxy}.'
         )
+        kwargs['dest_gt'] = self.mask.raster.GetGeoTransform()
         self.data[name] = datasource.get_by_extent(
             minx, miny, maxx, maxy, self.crs, **kwargs
         )
@@ -98,10 +250,10 @@ class RegionOfInterest(object):
                 for year in self.data[name].range():
                     self.data[name][year].dataset = callback(self.data[name][year].dataset, self.logger, **kwargs)
 
-
-
-    def export_dataset(self, where, name):
-        self.data[name].save(where)
+    def export_dataset(self, where, name, **kwargs):
+        """Exports a item in `data` to a file (TEMDataset) or files (Timeseries)
+        """
+        self.data[name].save(where, **kwargs)
        
     def export_boundary(self, where):
         self.boundary.to_file( where )
@@ -129,14 +281,16 @@ class RegionOfInterest(object):
 
         manifest = Manifest()
 
+        where.mkdir(exist_ok=True, parents=True)
+
         self.export_boundary(where / boundary_filename)
         manifest['boundary'] = boundary_filename
         self.export_mask( where/ mask_filename )  
         manifest['mask'] = mask_filename
 
-        for name in self.data:
+        for name in to_save:
             ds_where = where / f'{name}.nc' 
-            self.export_dataset(ds_where, name)
+            self.export_dataset(ds_where, name, **kwargs)
             manifest['data'][name] = f'{name}.nc' 
 
         manifest_file = where / manifest_filename
@@ -147,10 +301,7 @@ class RegionOfInterest(object):
             manifest['data'] = man_data
 
         manifest.to_file(manifest_file)
-
-
-
-
+        return manifest
 
     ## much of he tile stuff should move here
 
@@ -260,9 +411,9 @@ class SubregionGenerator(object):
         
         gdal.Warp(new_mask, self.full_region.mask.raster)
 
-        subregion = RegionOfInterest(self.tile_index.loc[[index]], new_mask)
+        subregion = Region(self.tile_index.loc[[index]], new_mask)
 
-        # for name, ds in self.full_region.data.items():
-            # subregion.import_and_normalize(...)
+        for name, ds in self.full_region.data.items():
+            subregion.import_from_datasource(name, ds)
 
         return subregion
